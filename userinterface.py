84f1b2dfc3596eb7678e6b1001da382d2a08ecdb
JordiIctech <42590692+JordiIctech@users.noreply.github.com>
2022-06-17 15:36:35 -0400

    User interface arrow key integration

--- userinterface.py
+++ userinterface.py
@@ -1,5 +1,54 @@
 print("-------------------------------------------User Interface----------------------------------------------")
+print("\n")
 
-print('\u25a1')
+from pynput import keyboard
 
-print('\u25a0')
+#Outer enter, empty box print("\u2610\n") 
+
+#Inner enter, filled box print("\u2612\n") 
+
+emptyballot = "    \u2610\n"
+
+xdballot = "    \u2612\n"
+
+ballots = [emptyballot, xdballot]
+
+options = [xdballot, emptyballot, emptyballot, emptyballot, emptyballot, emptyballot, emptyballot]
+
+def on_press(key):
+    on_press.currentkey = key
+    return False
+
+def listenOn():
+    with keyboard.Listener(
+            on_press=on_press) as listener:
+        listener.join()
+
+
+# ---------------------------------------------------------- Using the definitions
+
+
+while True:
+    print("____________________________________________HELLO USER_____________________________________________\n")
+    
+    for i in range(0,len(options)):
+        print(options[i])
+
+    listenOn()
+
+    if on_press.currentkey == keyboard.Key.esc:
+        break
+
+
+    else:
+        if on_press.currentkey == keyboard.Key.down:
+            options.insert(0,emptyballot)
+            options.pop()
+
+        if on_press.currentkey == keyboard.Key.up:
+            options.append(emptyballot)
+            options.pop(0)
+
+        for i in range(0,len(options)):
+            print(options[i])
+
