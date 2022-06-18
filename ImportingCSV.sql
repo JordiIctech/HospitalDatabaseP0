ea52c6c5af35a086957e0be424438cb83a68331a
JordiIctech <42590692+JordiIctech@users.noreply.github.com>
2022-06-18 04:16:47 -0400

    Import functionality working

---
+++ ImportingCSV.sql
@@ -0,0 +1,18 @@
+use project0database;
+
+DROP TABLE IF EXISTS patients;
+CREATE TABLE patients (
+    id INT NOT NULL AUTO_INCREMENT,
+    firsname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+LOAD DATA INFILE "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/GeneratedMedicalData.csv" 
+INTO TABLE patients 
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+SELECT * FROM patients;
