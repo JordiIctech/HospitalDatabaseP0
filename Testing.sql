b477f50a766ab78812ddb2d1b74de28266084f74
JordiIctech <42590692+JordiIctech@users.noreply.github.com>
2022-06-18 04:20:16 -0400

    Imports tested

--- Testing.sql
+++ Testing.sql
@@ -1,13 +1,4 @@
-DROP TABLE IF EXISTS Testing;
+USE project0database;
 
-CREATE TABLE Testing (
-ID int NOT NULL,
-PersonID int,
-LastName varchar(255),
-FirstName varchar(255),
-Age int,
-PRIMARY KEY (ID)
-);
 
-INSERT INTO Testing (ID,PersonID,LastName,FirstName,Age)
-VALUES (1,143,"west",'Gerogios',31), (2,267,"Ola","Hansen",30),(3,395,'Svendson','Tove',23);
+LOAD DATA LOCAL INFILE "GeneratedMedicalData.csv" INTO TABLE patients;
